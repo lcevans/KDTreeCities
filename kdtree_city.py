676409b665ce73d9b01648344e618c5d371405e5
Lawrence Evans <evanslc@missouri.edu>
2013-05-12 14:16:14 -0500

    Version 1.0 completed.

--- kdtree_city.py
+++ kdtree_city.py
@@ -1,31 +1,23 @@
-#This program starts by generating a number of cities located at random coordinates.
-#Then the program asks for a location (for now by text input, in the future by clicking the location)
-#The program then identifies the closest city (ideally graphically somehow). It does so *not* by taking the minimum of all distances (which is O(n))
-#but rather first builds a kd-tree (k-dimensional tree) which is a decision-tree which has the cities as leaves and each interior node
-#has a "branching rule". A point queried in the future is placed at the root of the tree, follows the branching rules, and arrives at a leaf which determines the
-#closest city. This algorithm is O(n log n) (at least on average). The branching rules are all of the form "x/y axis is less than/greater than a".
-#The tree is constructed recursively by dividing the collection of remaining cities along an axis by the median value of that axis for the cities. The choice of axis is
-#given by the depth in the tree (mod 2). When a colection consists of only one city, a leaf node is made for that city.
-
-#Remaining issues
-#1)Graphical interface: It should be that the cities are displayed graphically as small circles. To query a location, the user should click a point. The closest city should be indicated
-#graphically somehow (perhaps by a line drawn to it from the clicked location?)
+#In this program n cities are placed at randomly generated locations. The user can then click at a location and an algorithm will locate the nearest city.
+#By implementing a kd-tree, the algorithm runs in O(log n) complexity rather than the naive O(n) algorithm of checking the distance to each city one by one and taking the minimum.
 #
-#2)Algorithm: The algorithm as it stands does not always find the city of closest Euclidean distance. The kd-tree divides the unit square into rectangles and when a point is queried it merely 
-#returns the city in the same rectangle as the queried point. Instead the algorithm should go back up the tree and check some of the neighboring boxes as well.
-#
-#3) It would be cool to optionally draw the boxes of the kd-tree!
-
+#Interface Notes:
+#-Hit 'a' to toggle the choice of algorithm between the O(log n) kd-tree and the O(n) simple linear search 
+#-Hit 'g' to toggle the grid on and off
+#-Hit 'c' to toggle "color generation". Each city is assigned a color, random points are chosen, and a line is drawn to the nearest city in that cities color. Given some time, the colors 
+# partition the space into colored regions closest to the respective cities.
+#-Use the left mouse button to identify the closest city by euclidean distance
+#-Use the right mouse button to identify the closest city by the kd-tree (if the grid is turned on this is the city in the same grid-rectangle as the mouse)
 
 import random
 import numpy
-import pygame #so that we can see some graphix yo!
-import math #for square root
+import pygame #For graphics and keyboard/mouse interface.
 
-
-scaling = 500                       #while internally everything is done on the unit square, this is scaled before being displayed graphically
-number_of_cities = 40
-display = False                        # Set if you want to display the grid.
+scaling = 1000                         #While internally everything is done on the unit square, everything is scaled before being displayed graphically
+number_of_cities = 50                 
+display = False                        #Toggles whether or not to display the grid.
+color_gen = False                      #Toggles "color generation" mode
+use_naive_algorithm = False            #Toggles which algorithm to use to compute the nearest city
 
 class kdNode:    #A node of the kd-tree
     def __init__(self,depth = 0):
@@ -41,7 +33,7 @@ class kdNode:    #A node of the kd-tree
         else:
             axis = self.depth%2
             median = numpy.median([item[axis] for item in points])
-            self.data = ['branch',median]                                #build the branching rule by the median of the points' values for the appropriate axis
+            self.data = ['branch',median]                                #Build the branching rule by the median of the points' values for the appropriate axis
             left_half = [item for item in points if item[axis] < median]
             right_half = [item for item in points if item[axis] >= median]
             self.left = kdNode(self.depth + 1)                           
@@ -50,38 +42,43 @@ class kdNode:    #A node of the kd-tree
             self.right.build_tree(right_half)
 
     def find_best(self, point):                #Input: a queried location. Output: the city location in the appropriate leaf of the tree **according to grid**
+                                               #This is an O(log n) algorithm.
         if self.data[0] == 'leaf': return self.data[1]
         if self.data[0] == 'branch':
             axis = self.depth%2
             if point[axis] < self.data[1]: return self.left.find_best(point)
             if point[axis] >= self.data[1]: return self.right.find_best(point)
-    def find_best_improved(self, point):                #Input: a queried location. Output: the city location in the appropriate leaf of the tree **according to euclidean distance**
+
+
+    def find_best_improved(self, point):       #Input: a queried location. Output: a tuple consisting of the (optimal distance)^2 
+                                               #and the city location in the appropriate leaf of the tree **according to euclidean distance**
+                                               #This is an O(log n) algorithm.
         if self.data[0] == 'leaf':
             best = self.data[1]
-            best_dist = math.sqrt((best[0]-point[0])**2+(best[1]-point[1])**2)
-            return (best_dist,self.data[1])
+            best_dist_squared = (best[0]-point[0])**2+(best[1]-point[1])**2
+            return (best_dist_squared,self.data[1])
         if self.data[0] == 'branch':
             axis = self.depth%2
             if point[axis] < self.data[1]:
-                (best_dist,best) = self.left.find_best_improved(point)
-                if best_dist < math.fabs(point[axis]-self.data[1]):
-                    return (best_dist,best)
+                (best_dist_squared,best) = self.left.find_best_improved(point)
+                if best_dist_squared < (point[axis]-self.data[1])**2:
+                    return (best_dist_squared,best)
                 else:
-                    (alternative_dist,alternative) = self.right.find_best_improved(point)
-                    if best_dist <= alternative_dist:
-                        return (best_dist,best)
+                    (alternative_dist_squared,alternative) = self.right.find_best_improved(point)
+                    if best_dist_squared <= alternative_dist_squared:
+                        return (best_dist_squared,best)
                     else:
-                        return (alternative_dist,alternative)
+                        return (alternative_dist_squared,alternative)
             elif point[axis] >= self.data[1]:
-                (best_dist,best) = self.right.find_best_improved(point)
-                if best_dist < math.fabs(point[axis]-self.data[1]):
-                    return (best_dist,best)
+                (best_dist_squared,best) = self.right.find_best_improved(point)
+                if best_dist_squared < (point[axis]-self.data[1])**2:
+                    return (best_dist_squared,best)
                 else:
-                    (alternative_dist,alternative) = self.left.find_best_improved(point)
-                    if best_dist <= alternative_dist:
-                        return (best_dist,best)
+                    (alternative_dist_squared,alternative) = self.left.find_best_improved(point)
+                    if best_dist_squared <= alternative_dist_squared:
+                        return (best_dist_squared,best)
                     else:
-                        return (alternative_dist,alternative)
+                        return (alternative_dist_squared,alternative)
                 
     def  display_inorder(self):               #Standard in-order display of a tree. Useful for debugging.
         '''Displays the nodes of the tree in order from smallest to largest'''
@@ -92,9 +89,11 @@ class kdNode:    #A node of the kd-tree
             self.right.display_inorder()
         return        
 
+#####Create the array of cities#####
+
 cities = []
 
-for i in range(number_of_cities):                    #Create the array of cities
+for i in range(number_of_cities):                    
     cities += [(random.random(),random.random())]    #Each of the cities is a tuple of randomly generated coordinates. The cities variable stores the cities as being in the unit square.
 
 #####build the kd-tree######
@@ -124,19 +123,32 @@ def display_grid(root,box_range):    #box_range consists of a triple (x_min,x_ma
     elif root.data[0] == 'branch':
         axis = root.depth % 2
         median = int(scaling * root.data[1])
-        if axis == 0:    # divides along x-axis so draw a vetical line at x = median
+        if axis == 0:    #Divides along x-axis so draw a vetical line at x = median
             pygame.draw.line(screen,(255,100,100),(median,y_min),(median,y_max))
             display_grid(root.left, (x_min,y_min,median,y_max))
             display_grid(root.right, (median,y_min,x_max,y_max))
-        if axis == 1:    # divides along y-axis so draw a horizontal line at y = median
+        if axis == 1:    #Divides along y-axis so draw a horizontal line at y = median
             pygame.draw.line(screen,(255,100,100),(x_min,median),(x_max,median))
             display_grid(root.left, (x_min,y_min,x_max,median))
             display_grid(root.right, (x_min,median,x_max,y_max))
 
+def draw_screen():
+        screen.fill((0,0,0))
+        if display: display_grid(root, (0,0,scaling,scaling))
+        for city_location in scaled_cities:
+            pygame.draw.circle(screen,(255,255,255),city_location,2,1)
+    
+def linear_search((x,y)):     #Simple O(n) linear search to find the closest city
+    closest = cities[0]
+    best_dist_squared = (cities[0][0]-x)**2+(cities[0][1]-y)**2
+    for city in cities:
+        new_dist_squared = (city[0]-x)**2+(city[1]-y)**2
+        if new_dist_squared < best_dist_squared:
+            best_dist_squared = new_dist_squared
+            closest = city
+    return closest
 
-if display: display_grid(root, (0,0,scaling,scaling))
 running = 1
-
 while running == 1:
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
@@ -145,15 +157,37 @@ while running == 1:
         (scaled_x,scaled_y) = event.pos
         x = float(scaled_x)/scaling                         
         y = float(scaled_y)/scaling
-#        closest = root.find_best((x,y))
-        (distance,closest) = root.find_best_improved((x,y))
+        if event.button == 1:
+            (distance_squared,closest) = root.find_best_improved((x,y))
+        if event.button == 3:
+            closest = root.find_best((x,y))
         closest = map(lambda elem: int(scaling*elem),closest)  #scale it
         pygame.draw.line(screen,(255,255,255),(scaled_x,scaled_y),closest)
+            
     elif event.type == pygame.MOUSEBUTTONUP:                    #on MOUSEBUTTONUP clear screen and redraw cities (thereby eliminating the line)
-        screen.fill((0,0,0))
-        if display: display_grid(root, (0,0,scaling,scaling))
-        for city_location in scaled_cities:
-            pygame.draw.circle(screen,(255,255,255),city_location,2,1)
+        draw_screen()
+    elif event.type == pygame.KEYDOWN:
+        if event.unicode == 'g':
+            display = not display
+            draw_screen()
+        if event.unicode == 'c':
+            color_gen = not color_gen
+            draw_screen()
+        if event.unicode == 'a':
+            use_naive_algorithm = not use_naive_algorithm
+    if color_gen:                                               #when color_gen is True, query random points and draw a colored line to the nearest city in the appropriate color.
+        (x,y) = (random.random(),random.random())
+        (scaled_x,scaled_y) = (int(scaling*x),int(scaling*y))
+        if not use_naive_algorithm:
+            (distance_squared,closest) = root.find_best_improved((x,y))
+        elif use_naive_algorithm:
+            closest = linear_search((x,y))
+        closest = map(lambda elem: int(scaling*elem),closest)  #scale it
+        R = (13*closest[0]+19*closest[1]) % 206 + 50           #use a hash function to pick a color based on city location
+        G = (5*closest[0]-11*closest[1]) % 206 + 50
+        B = (47*closest[0]) % 206 + 50
+        color = (R,G,B)
+        pygame.draw.line(screen,color,(scaled_x,scaled_y),closest)       
     pygame.display.update()
 
     
